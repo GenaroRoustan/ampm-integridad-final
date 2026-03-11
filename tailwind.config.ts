2c13f7fd54dfadf7348b951171df5e9c8d745508
GenaroRoustan <genaro.roustan@ampmcentroamerica.com>
2026-03-10 18:23:56 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,10 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
+  safelist: [
+  'bg-blue-100', 'text-blue-700',
+  'bg-green-100', 'text-green-700',
+],
   theme: {
     container: {
       center: true,
